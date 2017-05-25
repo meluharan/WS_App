d4552568fe7e6ded76d148031f920d5917991ee7
meluharan <eluharan@gmail.com>
2017-05-25 20:47:29 +0300

    update

---
+++ app.js
@@ -0,0 +1,67 @@
+'use strict';
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const http = require('http').createServer(app); // try: Server(app)
+const io = require('socket.io')(http);
+const moment = require('moment');
+
+app.use(express.static('static'));
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true,
+}));
+
+app.use(function(req, res, next) {
+    console.log('Time:', moment(Date.now()));
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+
+function initRequestHandlers() {
+    app.get('/', function(req, res) {
+        console.log('-> index.html');
+        res.sendFile(__dirname + '/index.html');
+    });
+}
+
+function initSockets() {
+    io.on('connection', function(socket) {
+        // Get session id of the connection
+        const socketid = socket.id;
+        console.log('a user connected with session id '+socket.id);
+        socket.on('disconnect', function() {
+            console.log('user disconnected');
+        });
+
+        // dynamic room, from https://gist.github.com/crtr0/2896891
+        socket.on('app_id', function(app_id) {
+            console.log('joining room "'+app_id+'"');
+            // app_id = room
+            socket.join(app_id);
+        });
+
+        socket.on('message', function(jsonMsg) {
+            console.log('msg : '+jsonMsg.str);
+            console.log('room: '+jsonMsg.app_id);
+            jsonMsg.socketid = socketid; // pad client id to response
+            io.sockets.in(jsonMsg.app_id).emit('message', jsonMsg);
+        });
+    });
+}
+
+function startServer() {
+    initRequestHandlers();
+    initSockets();
+
+    http.listen(3000, function() {
+        console.log('Server started (3000)');
+    });
+}
+
+startServer();
+
+
---
+++ static/chat/chat.js
@@ -0,0 +1,66 @@
+'use strict';
+/* global io $ document window */
+
+/**
+ * @param {string} app_id the application identifier send to the server
+ */
+function initSocket(app_id) {
+    console.log('using hostname: '+window.location.hostname);
+    // Connect to server
+    const socket = io.connect('http://localhost:3000');
+    //const socket = io(); we could have it also by not specifying any URL when we call io(),
+    // since it defaults to trying to connect to the host that serves the page.
+    let sessionid;
+    // register room to the server
+    socket.on('connect', function() {
+        console.log('socket.io connected!');
+        sessionid = socket.io.engine.id;
+        socket.emit('app_id', app_id);
+    });
+
+    // on response
+    socket.on('message', function(data) {
+        let sender = data.socketid;
+        if(data.socketid === sessionid) {
+            sender = 'you';
+        }
+        console.log('message from '+sender+': '+JSON.stringify(data));
+
+        $('#messages').append(data.client_name+' says "'+data.str+'"</br>');
+    });
+
+    // sending messages
+    $('#message').bind('keypress', function(e) {
+        const code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 13 && socket) { // keyboard Enter
+            const msg = {};
+            msg.client_name = $('#name').val();
+            msg.app_id = app_id;
+            msg.time = Date.now();
+            msg.str = $('#message').val();
+            socket.json.emit('message', msg);
+            $('#message').val('');
+        }
+    });
+}
+
+/** @function */
+function initNameCheck() {
+    $('#message').prop('disabled', true);
+    $('#message').css({'background-color': 'grey'});
+
+    $('#name').on('change paste keyup', function() {
+        if($('#name').val()) {
+            $('#message').prop('disabled', false);
+            $('#message').css({'background-color': 'white'});
+        } else {
+            $('#message').prop('disabled', true);
+            $('#message').css({'background-color': 'grey'});
+        }
+    });
+}
+
+$(document).ready(function() {
+    initNameCheck();
+    initSocket('chat_app');
+});
